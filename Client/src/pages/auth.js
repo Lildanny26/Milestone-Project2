0003e4635e8f0946c430f23a343779040f47e12a
Your GitHub Username <davilama@unlv.nevada.edu>
2024-07-28 15:11:26 -0700

    updated buttons for l and R

--- Client/src/pages/auth.js
+++ Client/src/pages/auth.js
@@ -1,4 +1,83 @@
-export const Auth = () => {
+import React, { useState } from "react";
 
-    return <div> Auth </div>
+export const Auth = () => {
+    return (
+        <div className="Auth"> 
+            <Login /> 
+            <Register /> 
+        </div>
+    );
 };
+
+const Login = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    return (
+        <Form 
+            username={username} 
+            setUsername={setUsername} 
+            password={password}
+            setPassword={setPassword}
+            label="Login"
+        />
+    );
+};
+
+const Register = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+
+    };
+    return (
+        <Form 
+            username={username} 
+            setUsername={setUsername} 
+            password={password}
+            setPassword={setPassword}
+            label="Register"
+            onSubmit={onSubmit}
+        />
+    );
+};
+
+const Form = ({
+    username, 
+    setUsername, 
+    password, 
+    setPassword, 
+    label, 
+    onSubmit, 
+}) => {
+    return (
+        <div className= "auth-container"> 
+        <form onSubmit={onSubmit}>
+            <h2> {label} </h2>
+            <div className="form-group">
+                <label htmlFor="username"> Username: </label>
+                <input 
+                type="text" 
+                id="username" 
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+                /> 
+            </div>
+            <div className="form-group">
+                <label htmlFor="password"> Password: </label>
+                <input 
+                type="text" 
+                id="password" 
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                /> 
+            </div>
+            <button type="submit"> {label} </button>
+        </form>
+     </div>
+    );
+};
+
