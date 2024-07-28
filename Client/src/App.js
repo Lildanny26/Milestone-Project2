debfbe1a2e17331249f87ff8642ca4e005286c56
Your GitHub Username <davilama@unlv.nevada.edu>
2024-07-28 14:07:55 -0700

    added navbar and css

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-//import { Navbar } from "./components/navbar";
 import { Auth } from "./pages/auth";
 import { CreateRecipe } from "./pages/create-recipe";
 import { Home } from "./pages/home";
 import { SavedRecipes } from "./pages/saved-recipes";
+import { Navbar } from "./components/navbar"
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-recipe" element={<CreateRecipe />} />
