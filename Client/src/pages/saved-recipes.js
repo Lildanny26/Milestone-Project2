b2a98bbe71a94c2216f4020893e19a2edb319ddf
Your GitHub Username <davilama@unlv.nevada.edu>
2024-07-29 15:30:34 -0700

    finished save recipes now need request to work

--- Client/src/pages/saved-recipes.js
+++ Client/src/pages/saved-recipes.js
@@ -1,4 +1,40 @@
-export const SavedRecipes = () => {
+import {useEffect, useState} from "react"
+import axios from "axios"
+import { useGetUserID } from "../hooks/useGetUserID";
 
-    return <div> SavedRecipes </div>
+export const SavedRecipes = () => {
+    const [savedRecipes, setSavedRecipes] = useState([]);
+    const userID = useGetUserID();
+
+    useEffect (() => {
+        const fetchSavedRecipe = async () => {
+            try{
+                const response = await axios.get(`http://localhost:3001/recipes/savedRecipes/${userID}`
+                );
+                setSavedRecipes(response.data.savedRecipes);
+            }   catch (err) {
+                console.log(err);
+            }
+        };
+            
+        fetchSavedRecipe();
+    }, []);
+
+    return <div> 
+        <h1> Saved Recipes </h1> 
+        <ul> 
+            {savedRecipes.map((recipe) => (
+                <li key={recipe._id}>
+                    <div>
+                        <h2>{recipe.name}</h2>
+                    </div>
+                    <div className="instructions">
+                        <p> {recipe.instructions} </p>
+                    </div>
+                    <img src={recipe.imageUrl} alt={recipe.name}/>
+                    <p> Cooking Time: {recipe.cookingTime} (minutes) </p>
+                </li>
+            ))}
+        </ul>
+    </div>
 };
